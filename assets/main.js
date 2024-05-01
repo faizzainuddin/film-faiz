f343599d467caa9304e1086a0f25bbf6738f5b7a
username_gitub_faizzainuddin <email.gitub_faizzainudin080204@gmail.com>
2024-05-01 20:54:39 +0700

    first commit

---
+++ assets/main.js
@@ -0,0 +1,112 @@
+// JavaScript file untuk proyek Rhythm Movie TV
+
+
+// Ahmad Faiz bertanggung jawab untuk bagian Film
+// Branch: FILM
+
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  speed: 1200,
+  autoplay: 
+  {
+    delay: 2000,
+  },
+  loop: true,
+  zoom: true,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+
+// Siswo Adi Nugroho bertanggung jawab untuk bagian Music
+// Branch: MUSIC
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: '-28',
+  speed:1200,
+  autoplay: 
+    {
+      delay: 1000,
+    },
+  loop: true,
+  zoom: true,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 80,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+// Haikal Ihza bertanggung jawab untuk bagian TV Show
+// Branch: TV
+
+
+
+
+// Jarwo bertanggung jawab untuk bagian Trending Banner
+// Branch: TRENDING
+// Owlcarousel
+
+
+
+// Febri bertanggung jawab untuk bagian Base Layout dan Sidebar
+// Branch: BASE, SIDEBAR, FOOTER
+
+
+//SIDEBAR
+  const sidebarToggleBtn = document.getElementById('sidebarToggle');
+  const sidebarToggleDesktopBtn = document.getElementById('sidebarToggleDesktop');
+  const sidebar = document.querySelector('.sidebar');
+
+  sidebarToggleBtn.addEventListener('click', () => {
+    if (sidebar.style.display === 'none') {
+      sidebar.style.display = 'block';
+    } else {
+      sidebar.style.display = 'none';
+    }
+  });
+
+  sidebarToggleDesktopBtn.addEventListener('click', () => {
+    if (sidebar.style.display === 'none') {
+      sidebar.style.display = 'block';
+    } else {
+      sidebar.style.display = 'none';
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var sidebar = document.querySelector('.sidebar');
+  
+    // Tambahkan event listener untuk sentuhan
+    sidebar.addEventListener('touchstart', function() {
+      sidebar.classList.add('touch');
+    });
+  
+    sidebar.addEventListener('touchend', function() {
+      sidebar.classList.remove('touch');
+    });
+  });
+  
